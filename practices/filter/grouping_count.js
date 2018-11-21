136cd585bef95f7f15e61b3c9fc459249f1e3b9a
Iker Suen <suenik@oocl.com>
2018-11-21 15:40:34 +0800

    completed grouping_count high-order function

--- practices/filter/grouping_count.js
+++ practices/filter/grouping_count.js
@@ -3,20 +3,22 @@
 function grouping_count(collection) {
 
   //implement here
-  let newCollection = {};
-
-  for(var i = 0; i < collection.length; i++){
-    if(collection[i] in newCollection){
-      newCollection[collection[i]] += 1;
-    }else{
-      newCollection[collection[i]] = 1;
-    }
-  }
-
-  return newCollection;
+  // let newCollection = {};
+  //
+  // for(var i = 0; i < collection.length; i++){
+  //   if(collection[i] in newCollection){
+  //     newCollection[collection[i]] += 1;
+  //   }else{
+  //     newCollection[collection[i]] = 1;
+  //   }
+  // }
+  //
+  // return newCollection;
 
   //implement here
-
+  let newCollection = {};
+  collection.forEach(n => n in newCollection ? newCollection[n] += 1 : newCollection[n] = 1);
+  return newCollection;
 
 }
 
--- practices/map/map_to_four_multiples_add_one.js
+++ practices/map/map_to_four_multiples_add_one.js
@@ -1,9 +1,12 @@
 'use strict';
 var map_to_four_multiples_add_one = function(collection){
-  for(var i = 0; i < collection.length; i++){
-    collection[i] = (collection[i] * 4) + 1;
-  }
-  return collection;
+  // for(var i = 0; i < collection.length; i++){
+  //   collection[i] = (collection[i] * 4) + 1;
+  // }
+  // return collection;
+
+  //implement high order function
+  return collection.map(n => (n * 4) + 1);
 };
 
 module.exports = map_to_four_multiples_add_one;
