e255c297bb38198431e0f5167f24cb6c5fad56ce
Iker Suen <suenik@oocl.com>
2018-11-21 14:45:05 +0800

    grouping_count for loop

--- practices/filter/grouping_count.js
+++ practices/filter/grouping_count.js
@@ -3,6 +3,20 @@
 function grouping_count(collection) {
 
   //implement here
+  let newCollection = {};
+
+  for(var i = 0; i < collection.length; i++){
+    if(collection[i] in newCollection){
+      newCollection[collection[i]] += 1;
+    }else{
+      newCollection[collection[i]] = 1;
+    }
+  }
+
+  return newCollection;
+
+  //implement here
+
 
 }
 
