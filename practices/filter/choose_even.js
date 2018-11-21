bc9f3d6d1fce8e2ae2adb7ef99de02509fb7ae7f
Iker Suen <suenik@oocl.com>
2018-11-21 14:08:42 +0800

    for loop done for choose_even

--- practices/filter/choose_even.js
+++ practices/filter/choose_even.js
@@ -1,8 +1,14 @@
 'use strict';
 
 function choose_even(collection) {
-
+  var newCollection = [];
   //implement here
+  for(let i = 0; i < collection.length; i++){
+    if(collection[i] % 2 === 0){
+      newCollection.push(collection[i]);
+    }
+  }
+  return newCollection;
 }
 
 module.exports = choose_even;
